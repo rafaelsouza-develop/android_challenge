feb6fc0f90c597846b38e98776ac9207bb229aba
Rafael Souza <rafael.ribeiro@southsystem.com.br>
2022-10-06 15:32:33 -0300

    feat: create implementation on repository to request movie list

--- app/src/main/java/com/itau/starwars_challenge/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/itau/starwars_challenge/data/repository/MovieRepositoryImpl.kt
@@ -4,10 +4,19 @@ import com.itau.starwars_challenge.data.service.MovieService
 import com.itau.starwars_challenge.data.ResultRequest
 import com.itau.starwars_challenge.domain.model.MovieEntity
 import com.itau.starwars_challenge.domain.repository.MovieRepository
+import com.itau.starwars_challenge.mapper.datatodomain.toDomain
 
 class MovieRepositoryImpl(private val movieService: MovieService): MovieRepository {
 
     override suspend fun getMovies(): ResultRequest<List<MovieEntity>> {
-        TODO("Not yet implemented")
+        val response = movieService.getMovies()
+        if(response.isSuccessful){
+            return ResultRequest.Success(response.body().let { movies ->
+                movies!!.map {
+                    it.toDomain()
+                }
+            })
+        }
+        return ResultRequest.Failure(Throwable("Error ${response.errorBody()} ${response.message()} "))
     }
 }
