5b7ef61c7807f93c607e775dd06ec366f53af6be
Rafael Souza <rafael.ribeiro@southsystem.com.br>
2022-10-06 15:06:11 -0300

    feat: create MovieEntity and Mapper to domain

---
+++ app/src/main/java/com/itau/starwars_challenge/domain/model/MovieEntity.kt
@@ -0,0 +1,10 @@
+package com.itau.starwars_challenge.domain.model
+
+data class MovieEntity(
+
+    var cover: String?,
+    var title: String?,
+    var director: String?,
+    var episodeId: String?,
+    var releaseDate: String?
+)
