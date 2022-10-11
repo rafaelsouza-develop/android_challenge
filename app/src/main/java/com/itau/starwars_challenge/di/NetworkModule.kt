69850447696fbbfd3d30d8a0bd20a70e476196b9
Rafael Souza <rafael.ribeiro@southsystem.com.br>
2022-10-11 09:54:42 -0300

    feat: add interceptor on retrofit instance

--- app/src/main/java/com/itau/starwars_challenge/di/NetworkModule.kt
+++ app/src/main/java/com/itau/starwars_challenge/di/NetworkModule.kt
@@ -1,16 +1,42 @@
 package com.itau.starwars_challenge.di
 
+import com.google.gson.Gson
 import com.itau.starwars_challenge.data.service.MovieService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+const val BASE_URL = "https://api.npoint.io/"
+
 val networkModule = module {
+
+    factory {
+        Gson()
+    }
+
+    single {
+        GsonConverterFactory.create(get<Gson>())
+    }
+
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(get<HttpLoggingInterceptor>())
+            .build()
+    }
     single {
         Retrofit.Builder()
-            .baseUrl("https://api.npoint.io/")
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(get<GsonConverterFactory>())
+            .baseUrl(BASE_URL)
+            .client(get())
             .build()
+    }
+
+    single {
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
     }
 
     single { get<Retrofit>().create(MovieService::class.java) }
