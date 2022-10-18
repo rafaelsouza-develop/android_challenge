5ee9b5fa1e493f8857d79267e47907fbadc48a96
Rafael Souza <rafael.ribeiro@southsystem.com.br>
2022-10-18 15:46:19 -0300

    feat:create resource string

--- app/src/main/java/com/itau/starwars_challenge/presentation/ui/home/MoviesAdapter.kt
+++ app/src/main/java/com/itau/starwars_challenge/presentation/ui/home/MoviesAdapter.kt
@@ -1,9 +1,12 @@
 package com.itau.starwars_challenge.presentation.ui.home
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.itau.starwars_challenge.R
+import com.itau.starwars_challenge.R.string
 import com.itau.starwars_challenge.databinding.ItemMovieBinding
 import com.itau.starwars_challenge.domain.model.MovieEntity
 
@@ -13,7 +16,7 @@ class MoviesAdapter(private val movies: List<MovieEntity>):RecyclerView.Adapter<
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        return ViewHolder(ItemMovieBinding.inflate(inflater, parent, false))
+        return ViewHolder(ItemMovieBinding.inflate(inflater, parent, false), parent.context)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -22,13 +25,13 @@ class MoviesAdapter(private val movies: List<MovieEntity>):RecyclerView.Adapter<
 
     override fun getItemCount() = movies.size
 
-     class ViewHolder(private val binding: ItemMovieBinding) : RecyclerView.ViewHolder(binding.root){
+     class ViewHolder(private val binding: ItemMovieBinding, private val context: Context) : RecyclerView.ViewHolder(binding.root){
          fun bind(item: MovieEntity){
              with(binding){
-                 txtTitle.text = item.title
+                 txtTitle.text =
+                     "${context.getString(string.lb_title)} ${item.episodeId}: ${item.title}"
                  txtDirector.text = item.director
                  txtRelease.text = item.releaseDate
-
                  Glide
                      .with(binding.root)
                      .load(item.cover)
